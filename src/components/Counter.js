e567b91d0bdd49c7b75aabbf6cfef4261443c196
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-11 09:42:20 +0200

    reducer switch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,48 @@
 // imports
 import css from './Counter.module.css';
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+const initCounterValues = { counterValue: 0 };
+
+// funkcija kurios pagalba atnaujinsim state
+function counterReducer(state, action) {
+  console.log('counterReducer fired. action is: ', action);
+
+  switch (action.type) {
+    case 'daugiau':
+      return { counterValue: state.counterValue + 1 };
+    case 'maziau':
+      return { counterValue: state.counterValue - 1 };
+    default:
+      console.log('type not found');
+      return state;
+  }
+}
 
 function Counter(props) {
   // Logic
-  const [counterValue, setCounterValue] = useState(0);
+  // const [counterValue, setCounterValue] = useState(0);
+  const [state, dispatch] = useReducer(counterReducer, initCounterValues);
 
   const handleIncrement = () => {
-    setCounterValue(counterValue + 1);
+    // setCounterValue(counterValue + 1);
+    dispatch({ type: 'daugiau' });
   };
 
   const handleDecrement = () => {
     // neleisti eiti zemiau nulio
+    if (state.counterValue <= 0) return;
+    // setCounterValue(counterValue - 1);
+    dispatch({ type: 'maziau' });
   };
 
   // jsx
   return (
     <div className={css.counter}>
       <h3>{props.children}</h3>
-      <h2>{counterValue}</h2>
+      <h2>{state.counterValue}</h2>
       <button onClick={handleIncrement}>Increment</button>
-      <button>Decrement</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
